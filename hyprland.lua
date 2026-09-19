839ccb0733b5e3140d5b0daadf2e1668ad873d97
Tawukt <yadnin.git@hotmail.com>
2026-09-20 00:59:52 +0200

    remove ssh agent from hyprland conf

--- hyprland.lua
+++ hyprland.lua
@@ -37,7 +37,6 @@ end)
 
 hl.env("XCURSOR_SIZE", "24")
 hl.env("HYPRCURSOR_SIZE", "24")
-hl.env("SSH_AUTH_SOCK", os.getenv("XDG_RUNTIME_DIR") .. "/ssh-agent.socket")
 
 
 --  ██████╗ ████████╗██╗  ██╗███████╗██████╗ 
