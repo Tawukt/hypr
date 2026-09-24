1457359b2da94a3cdb23eece744b71426507a83a
Tawukt <yadnin.git@hotmail.com>
2026-09-25 01:12:47 +0200

    don't need option

--- keybindings.lua
+++ keybindings.lua
@@ -44,7 +44,7 @@ hl.bind(d.mainMod .. " + F1", hl.dsp.exec_cmd(
   "wl-paste | feh -"
 ))
 hl.bind("ALT + F3", hl.dsp.exec_cmd(
-  d.script .. "/ocrmath.sh \"python/OCRMath/main_p2t.py\""
+  d.script .. "/ocrmath.sh"
 ))
 hl.bind(d.mainMod .. " + SHIFT + W", hl.dsp.exec_cmd(
   "pkill waybar || waybar"
